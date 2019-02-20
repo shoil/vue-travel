b234ccb9fd6d48b90a8df6dc013a84c56789a6eb
shoil <2675553264@qq.com>
2019-02-20 15:35:20 +0800

    01

---
+++ precache-manifest.23948d575e097be4557b8d2396fe957c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "af23daabb0b2f25c7fbe",
+    "url": "/js/chunk-vendors.2043c055.js"
+  },
+  {
+    "revision": "fc3f8e494e51f9087e7c",
+    "url": "/js/app.7fffba85.js"
+  },
+  {
+    "revision": "8719fe4affdedf3886b4da92c480bce0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b7d9c4063ab6a6dab88089580571008",
+    "url": "/img/iconfont.3b7d9c40.svg"
+  },
+  {
+    "revision": "ee758fdde1de7e7fc1004b6aa917df0d",
+    "url": "/img/detail.ee758fdd.png"
+  },
+  {
+    "revision": "af23daabb0b2f25c7fbe",
+    "url": "/css/chunk-vendors.b32848a0.css"
+  },
+  {
+    "revision": "fc3f8e494e51f9087e7c",
+    "url": "/css/app.b6115320.css"
+  }
+];
